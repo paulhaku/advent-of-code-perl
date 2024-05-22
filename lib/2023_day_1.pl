ebc64cba28501e4ccfb50139a918e94fe3a79507
Haku <tenthdescent@gmail.com>
2024-05-21 21:17:05 -0400

    Clean up code

--- lib/2023_day_1.pl
+++ lib/2023_day_1.pl
@@ -7,28 +7,17 @@ my $filename = "input.txt";
 open my $fh, "<", $filename;
 
 my $sum = 0;
-while (!eof $fh) {
-    my $line = readline $fh;
-    # trim each line
-    $line =~ s/^\s+|\s+$//g;
+while (my $line = <$fh>) {
+    chomp $line;
+
     # extract first and last digit
-    my $first_digit;
-    my $last_digit;
+    my ($first_digit) = $line =~ /(\d)/;
+    my ($last_digit) = $line =~ /(\d)(?=\D*$)/;
 
-    if ($line =~ /(\d)/) {
-        $first_digit = $1;
-    }
-    if ($line =~ /(\d)(?=\D*$)/) {
-        $last_digit = $1;
-    }
+    # use the first digit twice if there's only one digit
+    $last_digit //= $first_digit;
 
-    if (defined $last_digit) {
-        print "$line ($first_digit, $last_digit)\n";
-        $sum += "${first_digit}${last_digit}";
-    } else {
-        print "$line ($first_digit)\n";
-        $sum += "${first_digit}${first_digit}";
-    }
+    $sum += "${first_digit}${last_digit}";
 }
 
 print $sum;
