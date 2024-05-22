f817eea390f9428e79946961f67d9d6b3713da42
Haku <tenthdescent@gmail.com>
2024-05-22 11:57:21 -0400

    Solve 2023 day 2 part 1

--- bin/2023_day2_part1.pl
+++ bin/2023_day2_part1.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
+use File::Spec;
+
+my $file_dir = File::Spec->catdir('input');
+my $filename = File::Spec->catfile($file_dir, '2023_day2.txt');
+open my $fh, "<", $filename;
 
 use constant {
     MAX_RED   => 12,
@@ -24,10 +29,21 @@ sub is_possible {
         $num_red //= 0;
         $num_blue //= 0;
         $num_green //= 0;
-        print "($num_red, $num_green, $num_blue)\n";
         if (($num_red > MAX_RED) || ($num_blue > MAX_BLUE) || ($num_green > MAX_GREEN)) {
             return 0;
         }
     }
     return 1;
 }
+
+my $id_sum = 0;
+while (my $line = <$fh>) {
+    chomp $line;
+    my ($game_id) = $line =~ /^Game (\d+?): .+$/;
+    if (is_possible($line)) {
+        $id_sum += $game_id;
+    }
+}
+
+print $id_sum, "\n";
+close $fh;
