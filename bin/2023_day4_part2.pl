868b0cd43765e78d07d7eef6ad3cc538e46e5698
Haku <tenthdescent@gmail.com>
2024-05-26 10:57:49 -0400

    Solve 2023 day 4 part 2

---
+++ bin/2023_day4_part2.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use File::Spec;
+use autodie;
+
+my $file_dir = File::Spec->catdir('input');
+my $file_name = File::Spec->catfile($file_dir, '2023_day4.txt');
+open my $fh, '<', $file_name;
+
+sub get_num_wins {
+    my ($winning_numbers_ref, $numbers_ref) = @_;
+    my @winning_numbers = @$winning_numbers_ref;
+    my @numbers = @$numbers_ref;
+
+    my $num_wins = 0;
+    foreach my $number (@numbers) {
+        next unless grep(/^$number$/, @winning_numbers);
+        $num_wins++;
+    }
+    return $num_wins;
+}
+
+my @card_copies = ();
+my $cards_by_id = {};
+
+while (my $line = <$fh>) {
+    chomp $line;
+    $line =~ /(\d+):\s+(.+?) \| (.+)/;
+    my $id = $1;
+    my @winning_numbers = split(/\s+/, $2);
+    my @numbers = split(/\s+/, $3);
+    $cards_by_id->{$id}->{"winning_numbers"} = \@winning_numbers;
+    $cards_by_id->{$id}->{"numbers"} = \@numbers;
+    $cards_by_id->{$id}->{"num_copies"} = 1;
+}
+close $fh;
+
+my $total_scratchcards = 0;
+
+foreach my $id (sort { $a <=> $b } keys %$cards_by_id) {
+    my @winning_numbers = @{$cards_by_id->{$id}->{"winning_numbers"}};
+    my @numbers = @{$cards_by_id->{$id}->{"numbers"}};
+    my $num_wins = get_num_wins(\@winning_numbers, \@numbers);
+    # Loop over every copy of the current card
+    for (my $i = 0; $i < $cards_by_id->{$id}->{"num_copies"}; $i++) {
+        # For every X wins this card has, make an extra copy of the next X cards
+        for (my $j = 1; $j <= $num_wins; $j++) {
+            my $copy_id = $id + $j;
+            $cards_by_id->{$copy_id}->{"num_copies"} += 1;
+        }
+    }
+    $total_scratchcards += $cards_by_id->{$id}->{"num_copies"};
+}
+
+print $total_scratchcards, "\n";
